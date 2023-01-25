a7b74ad7a889603eb98934046fc5b79d97d79ca0
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2023-01-24 22:44:53 -0300

    criado o component de Header

--- App.tsx
+++ App.tsx
@@ -6,13 +6,20 @@ import { Loading } from '@components/Loading';
 import { Groups } from "@screens/Groups";
 
 import theme from '@theme/index';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ?
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      
+      {fontsLoaded ?
         <Groups /> :
         <Loading/>
       }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,5 +4,4 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({theme}) => theme.COLORS.GRAY_600};
     align-items: center;
-    justify-content: center;
 `
